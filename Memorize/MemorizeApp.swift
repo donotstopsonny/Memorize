970b8c107fa6fc87d96ab487f35889ec022998dc
Hyeongchae Son <3076270s@gmail.com>
2024-05-19 18:21:13 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Hyeongchae Son on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
